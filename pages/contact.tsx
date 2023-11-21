70ccf7f001d609267f51988a2cfa5bb76146dbcb
Sirak Shrestha <sirakstha10@gmail.com>
2023-11-21 18:02:53 +0545

    added contact us page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,13 +1,63 @@
+// pages/contact-us.tsx
+
 import React from 'react';
+import AppLayout from "../components/AppLayout";
 import Navbar from '../components/Navbar/navbar';
+
 
 const Contact: React.FC = () => {
   return (
-    <div>
-      <Navbar/>
-      <h1>Contact SirakDai</h1>
-    </div>
+    <AppLayout>
+      <div className="container mx-auto py-8">
+        <h1 className="text-3xl font-bold mb-4">Contact Us</h1>
+        <p className="text-gray-500 mb-6">
+          Have questions, suggestions, or just want to say hello? Reach out to us!
+        </p>
+        <form>
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
+            Your Name
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="name"
+            type="text"
+            placeholder="Enter Your Name"
+          />
+          <div className="mt-4" />
+
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+            Your Email
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="email"
+            type="email"
+            placeholder="Enter Your Email"
+          />
+          <div className="mt-4" />
+
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="message">
+            Message
+          </label>
+          <textarea
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="message"
+            rows={4}
+            placeholder="Enter Your Message"
+          />
+          <div className="mt-6" />
+
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Send Message
+          </button>
+        </form>
+      </div>
+      </AppLayout>
   );
 };
 
 export default Contact;
+
