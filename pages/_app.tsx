4ad586a1e7761efc1deaaa6ea767dddd66856c7f
anujkarn002 <anujkarn002@gmail.com>
2023-11-25 15:05:54 +0545

    :zap: add Font Awesome styles import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import Header from '../components/header'; // Make sure the import is correct
 import { Header as HeaderData } from '../constants'; // Import your Header data
 import '../styles/globals.scss'; // Import your global styles here
 import 'mapbox-gl/dist/mapbox-gl.css';
+import '@fortawesome/fontawesome-svg-core/styles.css';
 import Footer from '../components/footer';
 
 function MyApp({ Component, pageProps }: AppProps) {
